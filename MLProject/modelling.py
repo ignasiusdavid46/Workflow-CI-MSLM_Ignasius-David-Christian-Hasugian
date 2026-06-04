dd6ed0a749003b5eaadfd291ae0f50d7119c6898
Ignasius David <ignasiusdavid046@gmail.com>
2026-06-04 12:44:49 +0700

    modif mlflow code

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -137,7 +137,10 @@ for k, v in metrics.items():
     print(f"  {k:22s}: {v:.4f}")
 
 # MLflow Manual Logging
-with mlflow.start_run(run_name="RF-CI-Advanced") as run:
+with mlflow.start_run(
+    run_name="RF-CI-Advanced",
+    nested=True
+) as run:
     run_id = run.info.run_id
 
     # Params
